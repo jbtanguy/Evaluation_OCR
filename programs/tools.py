06fa03190ae03fb9e4780a0813067c08200e9383
Jean-Baptiste Tanguy <jbtanguy56@gmail.com>
2022-09-29 07:26:46 +0200

    Delete tools.py

--- programs/tools.py
+++
@@ -1,8 +0,0 @@
-from optparse import OptionParser
-
-def get_args():
-	parser = OptionParser()
-	parser.add_option("-d", "--data_dir", help="path to htmls files to evaluate")
-	parser.add_option("-l", "--level", help="'page' or 'line")
-	(options, args) = parser.parse_args()
-	return options
